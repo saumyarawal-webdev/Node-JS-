23a33b70dff3df061967c2e5bf0864be3517cc9b
Saumya Rawal <rawalsaumya22@gmail.com>
2024-12-26 22:00:15 +0530

    Begins..!!

---
+++ 01 Hello World/file.js
@@ -0,0 +1,2 @@
+const fs = require("fs");
+fs.writeFileSync("./test.py", 'print("Hello Python")');
---
+++ 01 Hello World/hello.js
@@ -0,0 +1,6 @@
+const { add, sub } = require("./math");
+
+console.log("Hello There");
+
+console.log(add(3, 2));
+console.log(sub(3, 2));
